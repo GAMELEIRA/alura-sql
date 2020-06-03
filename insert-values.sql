acd8f70a7f5fa3fbc418bba27a92c40998f400b0
GAMELEIRA <gameleira270499@gmail.com>
2020-06-03 02:45:24 -0300

    Criado arquivo insert-values que têm como objetivo apresentar como montar querys de inserção nas tabelas no MySql

---
+++ insert-values.sql
@@ -0,0 +1,24 @@
+USE SUCOS;
+
+INSERT INTO PRODUTOS(
+COD_PRODUTO, 
+NOME,
+EMBALAGEM, 
+TAMANHO,
+SABOR,
+PRECO_LISTA
+) VALUES (
+"1040107",
+"Light - 350 ml - Melância",
+"Lata",
+"350 ml",
+"Melância",
+4.56
+), (
+"1037797",
+"Clean - 2 Litros - Laranja",	
+"PET",
+"2 Litros",
+"Laranja",
+16.01
+);
